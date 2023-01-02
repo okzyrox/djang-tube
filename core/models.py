9d6a125852a9aff09ae9f92049515d70f84b8d73
ZyroX <79484310+okzyrox@users.noreply.github.com>
2023-01-02 13:31:47 +0000

    change how videos are uploaded; now they go into specific directories for each user, and are assigned via their uuid instead of filename

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 import uuid
+import os
 
 # Create your models here.
 
@@ -34,7 +35,12 @@ class videoGameTopicGame(models.Model):
     def __str__(self):
         return self.game_name
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/video/user_<id>/<video_uuid>.<extension:mp4>
+    file, fileext = os.path.splitext(filename)
+    return 'video/user_{0}/{1}'.format(instance.video_creator.id, str(instance.video_uuid) + fileext)
 
+    # removes the overlapping filenames problem as each video is stored by as <uuid>.mp4 in user_<id>
 class videoObject(models.Model):
     video_uuid = models.UUIDField(editable=False, default=uuid.uuid4, unique=True)
 
@@ -42,10 +48,10 @@ class videoObject(models.Model):
 
     video_title = models.CharField(max_length=250)
     video_description = models.TextField(max_length=500, null=True, blank=True)
-    video_topic = models.ForeignKey(videoTopic, on_delete=models.SET_NULL, null=True)
-    video_gametopic_game = models.ForeignKey(videoGameTopicGame, on_delete=models.SET_NULL, null=True)
+    video_topic = models.ForeignKey(videoTopic, on_delete=models.SET_NULL, null=True, blank=True)
+    video_gametopic_game = models.ForeignKey(videoGameTopicGame, on_delete=models.SET_NULL, null=True, blank=True)
 
-    rawvideo = models.FileField(upload_to='video/', null=True, verbose_name="")
+    rawvideo = models.FileField(upload_to=user_directory_path, null=True, verbose_name="")
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
